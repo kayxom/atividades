527e2e96916306b92dc257e26f3a37e41d6b862c
falabricios <pereirajonatas0206@gmail.com>
2023-09-19 11:46:31 -0300

    Initial commit

---
+++ exe.kayke.py
@@ -0,0 +1,8 @@
+v1=(input("digite o primeiro numero "))
+v2=(input("digite o segundo numero"))
+s = v1 + v2
+# aqui e a soma de dois numeros 
+# -> comentario no python
+print("o resutado entre",v1 ,'e',v2,'e',s)
+print("o valor entre {0} e {1} o resutado e {2}".format(v1,v2,s))
+
---
+++ exe.py
@@ -0,0 +1,10 @@
+s= input ('digite alguma coisa;')
+print('digite alguma coisa;')
+print('tem espaco' , s.isspace())
+print('e nuerico',s.isnumeric ())
+print('e alfabetico',s.isalpha())
+print('e alfanumerico',s.isalnum())
+print('e maiuscula ', s.isupper())
+print('e meniscula ', s.islower())
+
+
